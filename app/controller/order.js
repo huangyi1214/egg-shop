6f98487e5765b68c7a8b4ab60a74832308e2bf3c
huangyi1214 <hy19841214@126.com>
2019-06-24 10:51:13 +0800

    RPC

--- app/controller/order.js
+++ app/controller/order.js
@@ -10,13 +10,15 @@ class OrderController extends Controller {
       if (ctx.request.headers['x-token']) {
         ctx.request.body.token = ctx.request.headers['x-token'];
       }
-      let result = await ctx.service.order.sendOrder(ctx.request.body);
-      // let result = await ctx.proxy.protoService.sendOrder(ctx.request.body);
+
+      // let result = await ctx.service.order.sendOrder(ctx.request.body);
+      let result = await this.app.proxy.protoService.sendOrder(ctx.request.body);
       console.log(JSON.stringify(result));
       result.ctx = ctx;
       ctx.app.io.emit('res', { code: 0, msgname: 'sendOrder', data: result.data, date: new Date() });
       ctx.helper.success(result);
     } catch (error) {
+      console.log(error);
       ctx.helper.success({ code: -999, msgname: 'sendOrder', message: '服务器异常' });
     }
 
